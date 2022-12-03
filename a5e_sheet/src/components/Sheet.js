3f9c401d1deda7e49e78d135397f4e388ddfe2df
Christian Gillies <ccrg4@hotmail.com>
2022-12-03 14:39:02 -0500

    Stable implementation

---
+++ a5e_sheet/src/components/Sheet.js
@@ -0,0 +1,22 @@
+
+function access()
+{
+    
+// const uri = "mongodb+srv://GameMaster:<password>@webdev.4wt0u2s.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+}
+function Sheet() {
+  return (
+    <div className="App">
+      
+      Sheet
+    </div>
+  );
+}
+
+export default Sheet;
