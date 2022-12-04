5f7805914a1e3c6c758721a19df39ac980527028
Christian Gillies <ccrg4@hotmail.com>
2022-12-04 00:20:02 -0500

    Webhosting json on site, renders data

--- a5e_sheet/src/components/Sheet.js
+++ a5e_sheet/src/components/Sheet.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import '../styles/Sheet.css';
 import Header from './Header';
-
+import Feature from "./Feature";
 function Sheet(props) {
-  const [data, setData] = useState("null");
+  const [data, setData] = useState(null);
   // Example link 
   //http://localhost:3200/db/findOne/test_database/test_collection/
   async function fetchData(query) {
-    const response = await fetch('https://raw.githubusercontent.com/christo480/FINAL-PROJECT_WEBDEV/main/Heritage/Dragonborn.json?token=GHSAT0AAAAAABYGN4JF2NOFHH5KOU52MYMWY4MFBRA');
+
+    
+    let response = await fetch('https://christo480.github.io/WEBDEV/Heritage/'+query+".json")
+    
+    let data = await response.json()
+    console.log(data)
     console.log(query)
-    //setData(await response.json());
+    
+    setData(data.Age);
   }
 
   
@@ -17,7 +23,7 @@ function Sheet(props) {
       <div className="sheet_App">
         <Header heritage={data} heritage_updater={fetchData} ></Header>
         <div className="sheet_body">
-            {data}
+            <Feature Age={data}></Feature>
         </div>
         
       </div>
