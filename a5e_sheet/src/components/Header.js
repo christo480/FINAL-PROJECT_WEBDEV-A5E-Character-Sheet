7c4f26618e4a18d71bc018e4bd34ffdabbccfdd7
Christian Gillies <ccrg4@hotmail.com>
2022-12-03 22:57:54 -0500

    Github Url secured

--- a5e_sheet/src/components/Header.js
+++ a5e_sheet/src/components/Header.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = "mongodb+srv://GameMaster:Jackoval@webdev.4wt0u2s.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-await client.connect()
 
-async function  get_heritage(heritage_query)
+
+function Header(props) {
+  
+async function get_heritage(event)
 {
-  const { db, collection } = req.params;
-  const result = await client.db(db).collection(collection).findOne({});
+ 
   //fetch call that pulls heritage data from mongoDB
   //should set react state webhook to update features dynamically
+
+  //console.log(event.target.value)//prints value of input
+  let input =event.target.value
+  props.heritage_updater(input)
 }
-function Header(props) {
     return (
       <div className="sheet_App">
-        <form onSubmit={thing(props.heritage)}>
-        <label for="heritage">Heritage:</label>
-        <input type="text" id="heritage" name="heritage"></input>
-        <input type="submit" value="Submit"></input>
+        <form>
+        <label>Heritage:</label>
+        <input type="text" id="heritage" name="heritage" onChange={get_heritage}></input>
+        
         </form>
       </div>
     );
--- a5e_sheet/src/components/Sheet.js
+++ a5e_sheet/src/components/Sheet.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import '../styles/Sheet.css';
 import Header from './Header';
 
-function Sheet() {
+function Sheet(props) {
+  const [data, setData] = useState("null");
+  // Example link 
+  //http://localhost:3200/db/findOne/test_database/test_collection/
+  async function fetchData(query) {
+    const response = await fetch('https://raw.githubusercontent.com/christo480/FINAL-PROJECT_WEBDEV/main/Heritage/Dragonborn.json?token=GHSAT0AAAAAABYGN4JF2NOFHH5KOU52MYMWY4MFBRA');
+    console.log(query)
+    //setData(await response.json());
+  }
+
+  
     return (
       <div className="sheet_App">
-        <Header></Header>
+        <Header heritage={data} heritage_updater={fetchData} ></Header>
         <div className="sheet_body">
-            Sheet Body
+            {data}
         </div>
         
       </div>
