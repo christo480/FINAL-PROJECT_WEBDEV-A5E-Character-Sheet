dfa15caf30ab623f4a9d94474c4569a9ae358d9d
Christian Gillies <ccrg4@hotmail.com>
2022-12-05 20:48:48 -0500

    Heritage Display

---
+++ a5e_sheet/src/components/Heritage_Section.js
@@ -0,0 +1,15 @@
+import Feature from "./Feature";
+import '../styles/Heritage_Section.css';
+function Heritage_Section(props) {
+    //A feature is a name and a description
+      return (
+        <div>
+          <Feature title="Name" value={props.Name}></Feature>
+          <Feature title="Age" value={props.Age}></Feature>
+          <Feature title="Size" value={props.Size}></Feature>
+          <Feature title="Speed" value={props.Speed}></Feature>
+        </div>
+      );
+    }
+  
+export default Heritage_Section;
