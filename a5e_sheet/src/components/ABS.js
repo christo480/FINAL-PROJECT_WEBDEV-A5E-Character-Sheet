4af60d5d65735b9bf90d155443edc100eba67135
Christian Gillies <ccrg4@hotmail.com>
2022-12-09 11:11:17 -0500

    StrScore styling

--- a5e_sheet/src/components/ABS.js
+++ a5e_sheet/src/components/ABS.js
@@ -1,14 +1,20 @@
 import Score from "./Score";
 import '../styles/ABS.css'
+import StrScore from "./StrScore"; 
 function ABS(props) {
     // Renders ability scores for character
       return (
         
         <div>
           <b>Ability Scores</b>
-          <div className="scores">{/*
+          <div className="scores">
             <StrScore name="Str" value={props.Str} setter={props.Str_setter}></StrScore>
-             <DexScore name="Dex" value={props.Dex}></DexScore>
+            <Score></Score>
+            <Score></Score>
+            <Score></Score>
+            <Score></Score>
+            <Score></Score>
+             {/*<DexScore name="Dex" value={props.Dex}></DexScore>
             <ConScore name="Con" value={props.Con}></ConScore>
             <IntScore name="Int" value={props.Int}></IntScore>
             <WisScore name="Wis" value={props.Wis}></WisScore>
--- a5e_sheet/src/components/StrScore.js
+++ a5e_sheet/src/components/StrScore.js
@@ -1,4 +1,5 @@
-import { DiceRoll, RollGroup } from '@dice-roller/rpg-dice-roller'; 
+import { DiceRoll, RollGroup } from '@dice-roller/rpg-dice-roller';
+import '../styles/StrScore.css'
 function StrScore(props) {
     //A feature is a name and a description
     function printChange(event)
@@ -7,17 +8,24 @@ function StrScore(props) {
       console.log(value)
       if (parseInt(value))
       {
+        
         props.setter(value)
       }
     }
+    function roll()
+    {
+      const roll =(new DiceRoll('1d20')).total+Math.floor((props.value-10)/2)
+      console.log(roll)
+      alert(props.name+" Roll "+roll)
+    }
       return (
         
-        <div>
-          
+        <div className='Score_section'>
           <b>{props.name}</b>
           <p >{props.value}</p>
           <input id={props.name} type={'text'} onChange={printChange}></input>
-          <i>{(props.value-10)/2}</i>
+          <i >{Math.floor((props.value-10)/2)}</i>
+          <button onClick={roll}> ROLL</button>
         </div>
       );
     }  
